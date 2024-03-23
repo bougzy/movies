a3752f0f64d73d27bfce774b278dc9613242ceca
Patrick Ubong Kingsley <47539464+bougzy@users.noreply.github.com>
2024-03-23 20:29:14 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,40 @@
 
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+// const cors = require('cors');
+
+// // MongoDB connection
+// mongoose.connect('mongodb+srv://hallmarkmanagement54:hallmarkmanagement54@hallmark.9xddgka.mongodb.net/hallmarkmanagement54');
+
+// // Allow requests from multiple routes on the frontend domain, including login and register routes
+// const allowedOrigins = [
+//   "https://hallmarkmoviesandmystries.vercel.app" // Remove the space before http
+//   // Add other origins as needed
+// ];
+
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true, // Allow cookies and credentials
+// }));
+
+// // Define routes for user and admin operations
+// app.use('/api/users', require('./routes/usersRoutes')); // This includes the existing routes
+// app.use('/api/deposits', require('./routes/depositRoutes')); // Include the new route for deposits
+
+// // Route for greeting message
+// app.get('/', (req, res) => {
+//   res.send('Welcome to our server!'); // Send a greeting message
+// });
+
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -9,7 +45,10 @@ mongoose.connect('mongodb+srv://hallmarkmanagement54:hallmarkmanagement54@hallma
 
 // Allow requests from multiple routes on the frontend domain, including login and register routes
 const allowedOrigins = [
-  "https://hallmarkmoviesandmystries.vercel.app" // Remove the space before http
+  "https://hallmarkmoviesandmystries.vercel.app",
+  "https://hallmarkmoviesandmystries.vercel.app/login",
+  "https://hallmarkmoviesandmystries.vercel.app/register",
+  "https://hallmarkmoviesandmystries.vercel.app/app"
   // Add other origins as needed
 ];
 
@@ -29,3 +68,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
